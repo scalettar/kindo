8e47994d70c310cac8deb99dabb827b1e94e7774
Daniel Scalettar <scalettar+github@gmail.com>
2020-02-04 22:05:14 -0800

    Deploy to heroku

--- src/themes/themes.styles.jsx
+++ src/themes/themes.styles.jsx
@@ -3,6 +3,7 @@ import { css } from "styled-components";
 // ------------------------THEMES---------------------
 // Use colorhexa to find new color schemes
 // ---------------------------------------------------
+
 // THEME: kindo (Default)
 const kindoBackground = css`
   background-color: #f7f1eb;
@@ -63,6 +64,30 @@ const kindoDarkP2 = css`
   background-color: #4db4ff;
 `;
 
+// THEME: andromeda
+const andromedaBackground = css`
+  background-color: #23262e;
+  /* background-color: #ffffff; */
+`;
+const andromedaEmpty = css`
+  box-shadow: 0 0 0 2px #333844 inset;
+  /* box-shadow: 0 0 0 2px #e1d4ca inset; */
+`;
+const andromedaNeutral = css`
+  background-color: #333844;
+  /* background-color: #f0e7df; */
+`;
+const andromedaShadow = css`
+  background-color: #333844;
+  /* background-color: #e1d4ca; */
+`;
+const andromedaP1 = css`
+  background-color: #00e8c6;
+`;
+const andromedaP2 = css`
+  background-color: #ff007a;
+`;
+
 // THEME: Cali
 const caliBackground = css`
   background-color: #ffffff;
@@ -102,6 +127,31 @@ const cyberP1 = css`
 const cyberP2 = css`
   background-color: #fcee09;
 `;
+
+// THEME: Super 
+// !!!WARNING!!! This theme is broken. Opacity of last played not visible.
+// Possible fix, add a flag in game-tile.styles.jsx for PlayedLastContainer
+// background-color based on theme property in this file like 
+// const <themeName>Last = css` {...}  `;
+const superBackground = css`
+  background-color: #ffffff;
+`;
+const superEmpty = css`
+  box-shadow: 0 0 0 2px #cccccc inset;
+`;
+const superNeutral = css`
+  background-color: #ededed;
+`;
+const superShadow = css`
+  background-color: #cccccc;
+`;
+const superP1 = css`
+  background-color: #101010;
+`;
+const superP2 = css`
+  background-color: #ff3c00;
+`;
+
 // ---------------------------------------------------
 
 export const getBackground = theme => {
@@ -109,6 +159,8 @@ export const getBackground = theme => {
   else if (theme === "cali") return caliBackground;
   else if (theme === "kindoDark") return kindoDarkBackground;
   else if (theme === "kindoLight") return kindoLightBackground;
+  else if (theme === "andromeda") return andromedaBackground;
+  else if (theme === "super") return superBackground;
   else return kindoBackground;
 };
 
@@ -117,6 +169,8 @@ export const getEmpty = theme => {
   else if (theme === "cali") return caliEmpty;
   else if (theme === "kindoDark") return kindoDarkEmpty;
   else if (theme === "kindoLight") return kindoLightEmpty;
+  else if (theme === "andromeda") return andromedaEmpty;
+  else if (theme === "super") return superEmpty;
   else return kindoEmpty;
 };
 
@@ -125,6 +179,8 @@ export const getNeutral = theme => {
   else if (theme === "cali") return caliNeutral;
   else if (theme === "kindoDark") return kindoDarkNeutral;
   else if (theme === "kindoLight") return kindoLightNeutral;
+  else if (theme === "andromeda") return andromedaNeutral;
+  else if (theme === "super") return superNeutral;
   else return kindoNeutral;
 };
 
@@ -133,6 +189,8 @@ export const getShadow = theme => {
   else if (theme === "cali") return caliShadow;
   else if (theme === "kindoDark") return kindoDarkShadow;
   else if (theme === "kindoLight") return kindoLightShadow;
+  else if (theme === "andromeda") return andromedaShadow;
+  else if (theme === "super") return superShadow;
   else return kindoShadow;
 };
 
@@ -141,6 +199,8 @@ export const getP1 = theme => {
   else if (theme === "cali") return caliP1;
   else if (theme === "kindoDark") return kindoDarkP1;
   else if (theme === "kindoLight") return kindoLightP1;
+  else if (theme === "andromeda") return andromedaP1;
+  else if (theme === "super") return superP1;
   else return kindoP1;
 };
 
@@ -149,5 +209,7 @@ export const getP2 = theme => {
   else if (theme === "cali") return caliP2;
   else if (theme === "kindoDark") return kindoDarkP2;
   else if (theme === "kindoLight") return kindoLightP2;
+  else if (theme === "andromeda") return andromedaP2;
+  else if (theme === "super") return superP2;
   else return kindoP2;
 };
